3cbc750bc4e8480d5197f98a9c41acfcab584cd1
marcelo1037 <marcelo_1037@hotmail.com>
2025-04-28 00:54:44 +0000

    Criação dos Movimentos xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Desafio de Xadrez - Marcelo
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+  
+    int i;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+            // Comando Bispo com WHILE
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    int bispo = 0;
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    i = 0;
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    while (i < 5) {
+        bispo++;
+        i++;
+    }
+    
+            // Comando Torre Com FOR
+    
+    int torre = 0;
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    for (i = 0; i < 5; i++){
+        torre++;
+    }
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+            // Comando Rainha com DO-WHILE
+
+    int rainha = 0;
+    
+        i = 0;
+    
+    do {
+        rainha++;
+        i++;
+    } while (i < 8);
+    
+
+            // Exibição dos resultados
+    
+        printf("Bispo: Movimentou cima, Direita! %d \n", bispo);
+        printf("Torre: Movimentou Casas a Direita! %d \n", torre);
+        printf("Rainha: Movimentou Casas a Esquerda! %d \n", rainha);
+    
+   
 
     return 0;
 }
